ea038a5c09e517c9fba34717f00ea6f13ab0c071
Kevin Eriksson <keerik@taltech.ee>
2025-03-04 09:46:06 +0200

    Added springboot files

---
+++ exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/ExchangeRateApiApplication.java
@@ -0,0 +1,13 @@
+package com.exchange_rates_app.exchange_rate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateApiApplication.class, args);
+	}
+
+}
---
+++ exchange-rate-api/src/test/java/com/exchange_rates_app/exchange_rate_api/ExchangeRateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange_rates_app.exchange_rate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
