828a478b2e1a717d568de4376e6b463d3a4ae161
Kevin Eriksson <keerik@taltech.ee>
2025-03-05 11:03:02 +0200

    ExchangeRateService now gets cubes correctly and saves them to the database

--- exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Services/ExchangeRateService.java
+++ exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Services/ExchangeRateService.java
@@ -1,17 +1,16 @@
 package com.exchange_rates_app.exchange_rate_api.Services;
 
-import com.exchange_rates_app.exchange_rate_api.Models.Cube;
-import com.exchange_rates_app.exchange_rate_api.Models.CubeRates;
-import com.exchange_rates_app.exchange_rate_api.Models.CurrencyRate;
-import com.exchange_rates_app.exchange_rate_api.Models.Envelope;
+import com.exchange_rates_app.exchange_rate_api.Models.*;
 import com.exchange_rates_app.exchange_rate_api.Repos.ExchangeRateRepo;
 import jakarta.xml.bind.JAXBException;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ExchangeRateService {
     private final ExchangeRateRepo exchangeRateRepo;
 
@@ -22,13 +21,13 @@ public class ExchangeRateService {
     public void saveCurrencyRates(String url) throws JAXBException, IOException {
         Envelope envelope = ExchangeRateFetcher.parseXML(url);
 
-        for (Cube cube : envelope.getCube()) {
-            LocalDate date = LocalDate.parse(cube.getTime());
+        for (CubeWrapper cubeWrapper : envelope.getCubeContainer().getCubeWrappers()) {
+            LocalDate date = LocalDate.parse(cubeWrapper.getTime());
 
-            for (CubeRates rateData : cube.getCubeRates()) {
+            for (CubeRates rateData : cubeWrapper.getCubeRates()) {
                 CurrencyRate currencyRate = new CurrencyRate();
                 currencyRate.setCurrency(rateData.getCurrency());
-                currencyRate.setRate(rateData.getRate());
+                currencyRate.setRate(Double.valueOf(rateData.getRate()));
                 currencyRate.setDate(date);
 
                 exchangeRateRepo.save(currencyRate);
