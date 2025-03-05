e87121b9c283674745f18367372e0c55f28215e4
Kevin Eriksson <keerik@taltech.ee>
2025-03-05 21:12:14 +0200

    Added get method to controller to fetch top5 growths

--- exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Controllers/ExchangeRateController.java
+++ exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Controllers/ExchangeRateController.java
@@ -1,12 +1,16 @@
 package com.exchange_rates_app.exchange_rate_api.Controllers;
 
+import com.exchange_rates_app.exchange_rate_api.Models.CurrencyGrowth;
 import com.exchange_rates_app.exchange_rate_api.Models.CurrencyRate;
 import com.exchange_rates_app.exchange_rate_api.Services.ExchangeRateScheduler;
 import com.exchange_rates_app.exchange_rate_api.Services.ExchangeRateService;
+import jakarta.xml.bind.JAXBException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,7 +21,6 @@ public class ExchangeRateController {
 
     private final ExchangeRateService exchangeRateService;
     private final ExchangeRateScheduler exchangeRateScheduler;
-
 
     @Autowired
     public ExchangeRateController(ExchangeRateService exchangeRateService, ExchangeRateScheduler exchangeRateScheduler) {
@@ -35,13 +38,9 @@ public class ExchangeRateController {
         Optional<CurrencyRate> currencyRate = Optional.ofNullable(exchangeRateService.getCurrencyRateById(id));
         return currencyRate.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
-    @PostMapping("/fetch")
-    public String fetchExchangeRates() {
-        try {
-            exchangeRateScheduler.fetchAndSaveRatesDaily();
-            return "Exchange rates successfully fetched and saved!";
-        } catch (Exception e) {
-            return "Error fetching exchange rates: " + e.getMessage();
-        }
+
+    @GetMapping("/top5")
+    public List<CurrencyGrowth> getTop5CurrencyGrowths() throws JAXBException, IOException {
+        return exchangeRateService.getTop5CurrencyMovers();
     }
 }
