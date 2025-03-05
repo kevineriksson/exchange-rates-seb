e4da04850ff4bf3109ab0bebf833d1735d4ae3f5
Kevin Eriksson <keerik@taltech.ee>
2025-03-05 21:12:49 +0200

    Removed n2 for loop and refactored methods

--- exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Services/ExchangeRateService.java
+++ exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Services/ExchangeRateService.java
@@ -13,13 +13,14 @@ import java.util.stream.Collectors;
 @Service
 public class ExchangeRateService {
     private final ExchangeRateRepo exchangeRateRepo;
+    private String url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml";
 
     public ExchangeRateService(ExchangeRateRepo exchangeRateRepo) {
         this.exchangeRateRepo = exchangeRateRepo;
     }
 
     public void saveCurrencyRates(String url) throws JAXBException, IOException {
-        Envelope envelope = ExchangeRateFetcher.parseXML(url);
+        Envelope envelope = ExchangeRateFetcher.fetchExchangeRates(url);
         CubeWrapper cubeWrapper = envelope.getCubeContainer().getCubeWrappers().get(0);
         LocalDate date = LocalDate.parse(envelope.getCubeContainer().getCubeWrappers().get(0).getTime());
 
@@ -42,24 +43,29 @@ public class ExchangeRateService {
         Optional<CurrencyRate> currencyRate = exchangeRateRepo.findById(id);
         return currencyRate.orElse(null);
     }
+    public List<CurrencyGrowth> getTop5CurrencyMovers() throws JAXBException, IOException {
+        return findTop5CurrencyMovers(url);
+    }
 
-    public List<CurrencyGrowth> get90DayCurrencyRates(String url) throws JAXBException, IOException {
-        Envelope envelope = ExchangeRateFetcher.parseXML(url);
+    public List<CurrencyGrowth> findTop5CurrencyMovers(String url) throws JAXBException, IOException {
+        Envelope envelope = ExchangeRateFetcher.fetchExchangeRates(url);
 
-        Map<String, List<CurrencyRate>> currencyRates = new HashMap<>();
-        for (CubeWrapper cubeWrapper : envelope.getCubeContainer().getCubeWrappers()) {
-            for (CubeRates rateData : cubeWrapper.getCubeRates()) {
-                currencyRates
-                        .computeIfAbsent(rateData.getCurrency(), k -> new ArrayList<>());
-            }
-        }
+        Map<String, List<CurrencyRate>> currencyRates = envelope.getCubeContainer().getCubeWrappers().stream()
+                .flatMap(cubeWrapper -> cubeWrapper.getCubeRates().stream())
+                .map(rateData -> {
+                    CurrencyRate currencyRate = new CurrencyRate();
+                    currencyRate.setRate(Double.parseDouble(rateData.getRate()));
+                    currencyRate.setCurrency(rateData.getCurrency());
+                    return currencyRate;
+                })
+                .collect(Collectors.groupingBy(CurrencyRate::getCurrency));
 
         ExchangeRateCalculator exchangeRateCalculator = new ExchangeRateCalculator();
         List<CurrencyGrowth> growthList = exchangeRateCalculator.calculateGrowth(currencyRates);
-        return getTop5CurrencyGrowth(growthList);
+        return getTop5CurrenciesBasedOnGrowth(growthList);
     }
 
-    private List<CurrencyGrowth> getTop5CurrencyGrowth(List<CurrencyGrowth> growthList) {
+    private List<CurrencyGrowth> getTop5CurrenciesBasedOnGrowth(List<CurrencyGrowth> growthList) {
         return growthList.stream()
                 .sorted((a, b) -> Double.compare(b.getGrowth(), a.getGrowth()))
                 .limit(5)
