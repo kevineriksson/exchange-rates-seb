6f6d2e23f9d6ba4e5f2606688f3020d8ccbadc49
Kevin Eriksson <keerik@taltech.ee>
2025-03-05 18:11:43 +0200

    Added service to fetch currency rates from api

--- exchange-rates-app/src/app/services/exchange-rate.service.ts
+++ exchange-rates-app/src/app/services/exchange-rate.service.ts
@@ -1,15 +1,32 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Injectable, signal} from '@angular/core';
+import {CurrencyRate} from '../CurrencyRate';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExchangeRateService {
 
-  private url = 'http://localhost:8080/api/currency-rates'
-  constructor(private http: HttpClient) {}
+  private url = 'http://localhost:8080/api/currencyRates'
+  constructor() {}
 
-  public getCurrencies() {
-    return this.http.get<any>(this.url);
+
+  fetchCurrencyRates(): Observable<CurrencyRate[]> {
+    return new Observable(observer => {
+      fetch(this.url)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to fetch currency rates');
+          }
+          return response.json();
+        })
+        .then(data => {
+          observer.next(data);
+          observer.complete();
+        })
+        .catch(error => {
+          observer.error(error);
+        });
+    });
   }
 }
