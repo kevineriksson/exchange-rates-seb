801b98297e1b46def320a8e46714f1e008d32d5d
Kevin Eriksson <keerik@taltech.ee>
2025-03-06 13:27:57 +0200

    Re-added getMovement

--- exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Models/CurrencyMovement.java
+++ exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Models/CurrencyMovement.java
@@ -1,14 +1,16 @@
 package com.exchange_rates_app.exchange_rate_api.Models;
 
 public class CurrencyMovement {
-    private final String currency;
-    private final Double movement;
+    private String currency;
+    private Double movement;
 
     public CurrencyMovement(String currency, Double growth) {
         this.currency = currency;
         this.movement = growth;
     }
-
+    public String getCurrency() {
+        return currency;
+    }
     public Double getMovement() {
         return movement;
     }
