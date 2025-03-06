38367774ae7e9ac9c559f6b2dcc7e353d62000a3
Kevin Eriksson <keerik@taltech.ee>
2025-03-06 12:43:52 +0200

    Cleanded code

--- exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Models/CurrencyMovement.java
+++ exchange-rate-api/src/main/java/com/exchange_rates_app/exchange_rate_api/Models/CurrencyMovement.java
@@ -1,8 +1,8 @@
 package com.exchange_rates_app.exchange_rate_api.Models;
 
 public class CurrencyMovement {
-    private String currency;
-    private Double movement;
+    private final String currency;
+    private final Double movement;
 
     public CurrencyMovement(String currency, Double growth) {
         this.currency = currency;
